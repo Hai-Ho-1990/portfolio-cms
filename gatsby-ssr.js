300d92fe42391fa3b49117a59e64e01ef0e87346
Hai-Ho-1990 <juno.hu2090@gmail.com>
2026-02-25 15:49:49 +0100

    Add google script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,27 @@
+import React from 'react';
+
 // gatsby-ssr.js
-// Ökar accessibility poäng
-export const onRenderBody = ({ setHtmlAttributes }) => {
+export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+    // Ökar accessibility poäng
     setHtmlAttributes({ lang: 'en' });
+
+    // Google Analytics (gtag.js)
+    setHeadComponents([
+        <script
+            key="gtag-src"
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-VBLZKENM4M"
+        />,
+        <script
+            key="gtag-config"
+            dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-VBLZKENM4M');
+                `,
+            }}
+        />,
+    ]);
 };
